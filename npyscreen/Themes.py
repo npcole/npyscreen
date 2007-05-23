5d2988b25ae192ef357155ccb97dc5d1756e5f0e
nicholas <nicholas@stav092.univ.ox.ac.uk>
2007-05-23 10:44:47 +0100

    Previous checkin broken.  NOW themes work.

---
+++ npyscreen/Themes.py
@@ -0,0 +1,21 @@
+import ThemeManager
+
+class DefaultTheme(ThemeManager.ThemeManager):
+    default_colors = {
+        'DEFAULT'     : 'WHITE_BLACK',
+        'FORMDEFAULT' : 'YELLOW_BLACK',
+        'NO_EDIT'     : 'BLUE_BLACK',
+        'STANDOUT'    : 'CYAN_BLACK',
+        'LABEL'       : 'BLUE_BLACK',
+        'CONTROL'     : 'GREEN_BLACK',
+    }
+
+class ColorfulTheme(ThemeManager.ThemeManager):
+    default_colors = {
+        'DEFAULT'     : 'RED_BLACK',
+        'FORMDEFAULT' : 'YELLOW_BLACK',
+        'NO_EDIT'     : 'BLUE_BLACK',
+        'STANDOUT'    : 'CYAN_BLACK',
+        'LABEL'       : 'BLUE_BLACK',
+        'CONTROL'     : 'GREEN_BLACK',
+    }
