af72ac400670c127328de6e23aa47985889464f0
nicholas <nicholas@Macintosh.local>
2008-10-04 19:20:04 +0100

    Added some additional files.

---
+++ TwoScreenTrial.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+TwoScreenTrial.py
+
+Created by Nicholas Cole on 2008-04-14.
+Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
