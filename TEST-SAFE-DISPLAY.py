86fb3709e1de8b6abf0f2f025a409836bb08db57
nicholas cole <nicholas.cole@gmail.com>
2012-04-07 15:50:30 +0100

    More unicode changes.

--- TEST-SAFE-DISPLAY.py
+++ TEST-SAFE-DISPLAY.py
@@ -15,6 +15,7 @@ class TestApp(npyscreen.NPSAppManaged):
         t1 = F.add(self.__class__.__TEXT_WIDGET, name = "Text:", )
         t2 = F.add(self.__class__.__TEXT_WIDGET, name = "Text:", )
         t3 = F.add(self.__class__.__TEXT_WIDGET, name = "Text:", )
+        t4 = F.add(self.__class__.__TEXT_WIDGET, name = "Text:", )
         
         m1 = F.add(npyscreen.MultiLine, name = "Mutliline", scroll_exit=True, max_height=5)
         
@@ -26,10 +27,14 @@ class TestApp(npyscreen.NPSAppManaged):
         t3.value = u"This is ∑ a test"
         t1.value = u"Testing tripple width \u3111 stuff."
         t2.value = u"Testing double width stuff \u1000 <- there"
+        t4.value = u"another test is \u1D656 this one."
         
-        m1.values = [t1.value, t2.value, t3.value]
+        m1.values = [t1.value, t2.value, t3.value, 
+                        'another test \u24AF is here',
+                        'another test is \u1D666 this one.']
         
-        me.value = '\n'.join([t1.value, t2.value, t3.value])
+        me.value = '\n'.join([t1.value, t2.value, t3.value,
+                                'another test is \u1D656 this one.'])
         
         
         # This lets the user play with the Form.
