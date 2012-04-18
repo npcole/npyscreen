168b11a1ceaa50452b65424ae5b0200e48aa1221
nicholas cole <nicholas.cole@gmail.com>
2012-04-18 13:07:32 +0100

    Minor bugfix.

--- EXAMPLE-multiple-screens.py
+++ EXAMPLE-multiple-screens.py
@@ -2,13 +2,16 @@ import npyscreen, curses
 
 class MyTestApp(npyscreen.NPSAppManaged):
     def onStart(self):
-        self.registerForm("MAIN", MainForm(name="Screen 1", color="IMPORTANT"))
-        self.registerForm("SECOND", MainForm(name="Screen 2", color="WARNING"))
+        self.addForm("MAIN", MainForm, name="Screen 1", color="IMPORTANT",)
+        self.addForm("SECOND", MainForm, name="Screen 2", color="WARNING",)
     
     def change_form(self, name):
         self.switchForm(name)
     
-class MainForm(npyscreen.Form):
+    def onExit(self):
+        print("Goodbye!")
+    
+class MainForm(npyscreen.ActionForm):
     def create(self):
         self.add(npyscreen.TitleText, name = "Text:", value= "Press ^T to change screens" )
         self.add_handlers({"^T": self.change_forms})
@@ -21,6 +24,9 @@ class MainForm(npyscreen.Form):
             change_to = "MAIN"
         self.parentApp.change_form(change_to)
         #self.editing=False
+    
+    def on_ok(self):
+        self.parentApp.switchForm(None)
     
 
 def main():
