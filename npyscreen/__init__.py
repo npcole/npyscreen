4836a0f2215aa98b3f524d31af1e6d6c7e47b815
nicholas cole <nicholas.cole@gmail.com>
2012-09-17 14:43:30 +0100

    Added additional FormMultiPage classes.

--- npyscreen/__init__.py
+++ npyscreen/__init__.py
@@ -9,18 +9,21 @@ from   .apNPSApplicationManaged import NPSAppManaged
 from   .proto_fm_screen_area    import setTheme
 from   .fmForm                  import FormBaseNew, Form, TitleForm, TitleFooterForm, SplitForm, FormExpanded, FormBaseNewExpanded
 from   .fmActionForm            import ActionForm, ActionFormExpanded
-from   .fmFormWithMenus         import FormWithMenus, ActionFormWithMenus, FormBaseNewWithMenus, SplitFormWithMenus
+from   .fmFormWithMenus         import FormWithMenus, ActionFormWithMenus, \
+                                       FormBaseNewWithMenus, SplitFormWithMenus
 from   .fmPopup                 import Popup, MessagePopup, ActionPopup, PopupWide, ActionPopupWide
 from   .fmFormMutt              import FormMutt, FormMuttWithMenus
 from   .fmFileSelector          import FileSelector
 
-from .fmFormMultiPage           import FormMultiPage
+from .fmFormMuttActive          import ActionControllerSimple, TextCommandBox, \
+                                       FormMuttActive, FormMuttActiveWithMenus
+
+from .fmFormMultiPage           import FormMultiPage, FormMultiPageAction,\
+                                       FormMultiPageActionWithMenus, FormMultiPageWithMenus
 
 from   .npysNPSTree             import NPSTreeData
 
 from .npysNPSFilteredData       import NPSFilteredDataBase, NPSFilteredDataList
-from .fmFormMuttActive          import ActionControllerSimple, TextCommandBox, \
-                                    FormMuttActive, FormMuttActiveWithMenus
 
 from .wgbutton                  import MiniButton
 from .wgbutton                  import MiniButtonPress
@@ -36,7 +39,8 @@ from .wgslider                  import Slider, TitleSlider
 from .wgwidget                  import DummyWidget, NotEnoughSpaceForWidget
 
 from .wgmultiline               import MultiLine, Pager, TitleMultiLine, MultiLineAction
-from .wgmultiselect             import MultiSelect, TitleMultiSelect, MultiSelectFixed, TitleMultiSelectFixed, MultiSelectAction
+from .wgmultiselect             import MultiSelect, TitleMultiSelect, MultiSelectFixed, \
+                                       TitleMultiSelectFixed, MultiSelectAction
 from .wgeditmultiline           import MultiLineEdit
 from .wgcombobox                import ComboBox, TitleCombo
 from .wgcheckbox                import Checkbox, RoundCheckBox
--- npyscreen/fmFormMultiPage.py
+++ npyscreen/fmFormMultiPage.py
@@ -1,6 +1,8 @@
 ## Very, very experimental. Do NOT USE.
 
 from . import fmForm
+from . import wgNMenuDisplay
+
 
 class FormMultiPage(fmForm.FormBaseNew):
     def __init__(self, *args, **keywords):
@@ -57,5 +59,76 @@ class FormMultiPage(fmForm.FormBaseNew):
                 if self._widgets__[n].editable and not self._widgets__[n].hidden: 
                     self.editw = n
                     break
+                    
+                    
+class FormMultiPageAction(FormMultiPage):
+    CANCEL_BUTTON_BR_OFFSET = (2, 12)
+    OK_BUTTON_TEXT          = "OK"
+    CANCEL_BUTTON_TEXT      = "Cancel"
     
+    def on_ok(self):
+        pass
     
+    def on_cancel(self):
+        pass
+    
+    def pre_edit_loop(self):
+        self._page_for_buttons = len(self._pages__)-1
+        self.switch_page(self._page_for_buttons)
+        
+        # Add ok and cancel buttons. Will remove later
+        tmp_rely, tmp_relx = self.nextrely, self.nextrelx
+        
+        c_button_text = self.CANCEL_BUTTON_TEXT
+        cmy, cmx = self.curses_pad.getmaxyx()
+        cmy -= self.__class__.CANCEL_BUTTON_BR_OFFSET[0]
+        cmx -= len(c_button_text)+self.__class__.CANCEL_BUTTON_BR_OFFSET[1]
+        self.c_button = self.add_widget(self.__class__.OKBUTTON_TYPE, name=c_button_text, rely=cmy, relx=cmx, use_max_space=True)
+        self._c_button_postion = len(self._widgets__)-1
+        self.c_button.update()
+        
+        my, mx = self.curses_pad.getmaxyx()
+        ok_button_text = self.OK_BUTTON_TEXT
+        my -= self.__class__.OK_BUTTON_BR_OFFSET[0]
+        mx -= len(ok_button_text)+self.__class__.OK_BUTTON_BR_OFFSET[1]
+        self.ok_button = self.add_widget(self.__class__.OKBUTTON_TYPE, name=ok_button_text, rely=my, relx=mx, use_max_space=True)
+        self._ok_button_postion = len(self._widgets__)-1
+        # End add buttons
+        self.nextrely, self.nextrelx = tmp_rely, tmp_relx
+        self.switch_page(0)
+        
+    def _during_edit_loop(self):
+        if self.ok_button.value or self.c_button.value:
+            self.editing = False
+    
+        if self.ok_button.value:
+            self.ok_button.value = False
+            self.edit_return_value = self.on_ok()
+        elif self.c_button.value:
+            self.c_button.value = False
+            self.edit_return_value = self.on_cancel()
+    
+    def post_edit_loop(self):
+        self.switch_page(self._page_for_buttons)
+        self.ok_button.destroy()
+        self.c_button.destroy()
+        del self._widgets__[self._ok_button_postion]
+        del self.ok_button
+        del self._widgets__[self._c_button_postion]
+        del self.c_button
+        #self.nextrely, self.nextrelx = tmp_rely, tmp_relx
+        self.display()
+        self.editing = False
+        
+        return self.edit_return_value
+
+
+class FormMultiPageWithMenus(fmForm.FormBaseNew):
+    def __init__(self, *args, **keywords):
+        super(FormMultiPageWithMenus, self).__init__(*args, **keywords)
+        self.initialize_menus()
+
+class FormMultiPageActionWithMenus(FormMultiPageAction, wgNMenuDisplay.HasMenus):
+    def __init__(self, *args, **keywords):
+        super(FormMultiPageActionWithMenus, self).__init__(*args, **keywords)
+        self.initialize_menus()
