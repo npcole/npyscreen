c66c9378339074c76acd79c43c082c6ce0ef81b9
nicholas cole <nicholas.cole@gmail.com>
2013-09-09 17:12:43 +0100

    Cannot move grid cursor into empty cells.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="2.0pre78",
+	version="2.0pre79",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
@@ -36,7 +36,7 @@ There is a mailing list available at https://groups.google.com/forum/?fromgroups
 
 
 *Latest Changes*:
-Version 2.0pre78 is a bugfix release.
+Version 2.0pre79 is a bugfix release.
 
 Version 2.0pre76 further improves the handling of mouse events on compatible
 terminals.
