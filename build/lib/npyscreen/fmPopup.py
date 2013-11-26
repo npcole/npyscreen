de9a8f84a5bfa132462b3d4bceff2a6b95f9e20c
nicholas cole <nicholas.cole@gmail.com>
2013-11-26 21:11:59 +0000

    Made the creation of custom sized forms easier.
    Fixed a bug in the mouse handling of multiline forms.

--- build/lib/npyscreen/fmPopup.py
+++ build/lib/npyscreen/fmPopup.py
@@ -7,16 +7,20 @@ import curses
 
 
 class Popup(fmForm.Form):
-    def __init__(self,
-        lines = 12, 
-        columns=60,
-        minimum_lines=None,
-        minimum_columns=None,
-        *args, **keywords):
-        super(Popup, self).__init__(lines = lines, columns=columns, 
-        *args, **keywords)
-        self.show_atx = 10
-        self.show_aty = 2
+    DEFAULT_LINES      = 12
+    DEFAULT_COLUMNS    = 60
+    SHOW_ATX           = 10
+    SHOW_ATY           = 2
+    #def __init__(self,
+    #    lines = 12, 
+    #    columns=60,
+    #    minimum_lines=None,
+    #    minimum_columns=None,
+    #    *args, **keywords):
+    #    super(Popup, self).__init__(lines = lines, columns=columns, 
+    #    *args, **keywords)
+    #    self.show_atx = 10
+    #    self.show_aty = 2
         
 class ActionPopup(fmActionForm.ActionForm, Popup):
     def __init__(self, *args, **keywords):
@@ -29,16 +33,20 @@ class MessagePopup(Popup):
         self.TextWidget = self.add(multiline.Pager, scroll_exit=True, max_height=self.widget_useable_space()[0]-2)
         
 class PopupWide(Popup):
-    def __init__(self,
-        lines = 14, 
-        columns=None,
-        minimum_lines=None,
-        minimum_columns=None,
-        *args, **keywords):
-        super(PopupWide, self).__init__(lines = lines, columns=columns, 
-        *args, **keywords)
-        self.show_atx = 0
-        self.show_aty = 0
+    DEFAULT_LINES      = 14
+    DEFAULT_COLUMNS    = None
+    SHOW_ATX           = 0
+    SHOW_ATY           = 0
+    #def __init__(self,
+    #    lines = 14, 
+    #    columns=None,
+    #    minimum_lines=None,
+    #    minimum_columns=None,
+    #    *args, **keywords):
+    #    super(PopupWide, self).__init__(lines = lines, columns=columns, 
+    #    *args, **keywords)
+    #    self.show_atx = 0
+    #    self.show_aty = 0
         
 class ActionPopupWide(fmActionForm.ActionForm, PopupWide):
     def __init__(self, *args, **keywords):
