ae1377ff7c4777362fa5aec0aec48f86c9dafac4
nicholas cole <nicholas.cole@gmail.com>
2014-04-09 11:30:42 +0100

    Sanity tests in titlefield.

--- build/lib/npyscreen/wgtitlefield.py
+++ build/lib/npyscreen/wgtitlefield.py
@@ -57,9 +57,12 @@ class TitleText(widget.Widget):
                 self._passon['width'] -= self.text_field_begin_at+1
         
         if self.use_two_lines:
-            if 'max_height' in self._passon:
+            if 'max_height' in self._passon and self._passon['max_height']:
+                if self._passon['max_height'] == 1:
+                    raise ValueError("I don't know how to resolve this: max_height == 1 but widget using 2 lines.")
                 self._passon['max_height'] -= 1
-            if 'height' in self._passon:
+            if 'height' in self._passon and self._passon['height']:
+                raise ValueError("I don't know how to resolve this: height == 1 but widget using 2 lines.")
                 self._passon['height'] -= 1
         
 
