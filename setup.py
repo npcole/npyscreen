69feb308a197977235c401e647d06b7506d488b7
nicholas cole <nicholas.cole@gmail.com>
2014-04-14 09:35:35 +0100

    Title.. widgets can now define when_value_edited()

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="3.37",
+	version="3.4",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
@@ -38,6 +38,10 @@ There is a mailing list available at https://groups.google.com/forum/?fromgroups
 
 
 *Latest Changes*:
+
+Version 3.5 when_value_edited defined on Title.. widgets now work as users expect.
+
+Version 3.4 Fixed bugs in Title.. widgets and in the App classes.
 
 Version 3.3 and the subsequent minor releases fix some bugs, mainly related
 to changes caused by allowing resized forms.
