4a25b9b4123bf6f98d8a65c22e8edb63030f267a
nicholas cole <nicholas.cole@gmail.com>
2014-04-30 16:53:55 +0100

    Box widgets now pass editable status to contained widget on init.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="3.6",
+	version="3.7",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
