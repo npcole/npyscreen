a2dceaca4b7b30a30d51856d0ebc272ae46cfe8e
nicholas cole <nicholas.cole@gmail.com>
2014-05-20 12:10:32 +0100

    Cleaned up an example.

--- EXAMPLE-BOX.py
+++ EXAMPLE-BOX.py
@@ -5,13 +5,11 @@ import npyscreen
 #npyscreen.disableColor()
 class TestApp(npyscreen.NPSApp):
     def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
         F = npyscreen.Form(name = "Welcome to Npyscreen",)
         t = F.add(npyscreen.BoxBasic, name = "Basic Box:", max_width=30, relx=2, max_height=3)
         t.footer = "This is a footer"
         
-        t1 =t = F.add(npyscreen.BoxBasic, name = "Basic Box:", rely=2, relx=32, 
+        t1 = F.add(npyscreen.BoxBasic, name = "Basic Box:", rely=2, relx=32, 
                         max_width=30, max_height=3)
         
         
