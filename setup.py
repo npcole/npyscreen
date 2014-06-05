5f73103958088ad635fa80372fd6c0f7dd3b9c5f
nicholas cole <nicholas.cole@gmail.com>
2014-06-05 07:13:58 +0100

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="4.1.1",
+	version="4.1.2",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
