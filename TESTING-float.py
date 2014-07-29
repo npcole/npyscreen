a1582bbe1c008609f864a708b3b22ca703a149c1
nicholas cole <nicholas.cole@gmail.com>
2014-07-29 12:38:42 +0100

    Experimenting.

--- TESTING-float.py
+++ TESTING-float.py
@@ -6,7 +6,7 @@ import npyscreen
 class TestApp(npyscreen.NPSApp):
     def main(self):
         F  = npyscreen.Form(name = "Welcome to Npyscreen",)
-        ms = F.add(npyscreen.Button, name="Button", max_width=7, rely = -10, relx = -15)
+        ms = F.add(npyscreen.Button, name="Button", max_width=7, rely = -5, relx = -13)
         ml = F.add(npyscreen.TitleMultiLine, name="Multiline", relx = -55, rely = 5, max_height=12, values = [1,2,3])
         
         F.edit()
