2d6f327e08e3a92390bf51b3df3d5a9c9c6b37b7
nicholas cole <nicholas.cole@gmail.com>
2014-09-04 09:50:10 +0100

    Updated the documentation. Added ActionFormMinimal. Release 4.4.0.

--- npyscreen/__init__.py
+++ npyscreen/__init__.py
@@ -11,7 +11,7 @@ from   .apNPSApplicationManaged import NPSAppManaged
 from   .proto_fm_screen_area    import setTheme
 from   .fmForm                  import FormBaseNew, Form, TitleForm, TitleFooterForm, SplitForm, FormExpanded, FormBaseNewExpanded
 from   .fmActionForm            import ActionForm, ActionFormExpanded
-from   .fmActionFormV2          import ActionFormV2, ActionFormExpandedV2
+from   .fmActionFormV2          import ActionFormV2, ActionFormExpandedV2, ActionFormMinimal
 from   .fmFormWithMenus         import FormWithMenus, ActionFormWithMenus, \
                                        FormBaseNewWithMenus, SplitFormWithMenus, \
                                        ActionFormV2WithMenus
