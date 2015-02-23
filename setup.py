9707f0f6ccd794f571e5c82c54a4a608ae7d1927
nicholas cole <nicholas.cole@gmail.com>
2015-02-23 12:13:38 +0000

    Version 4.8.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="4.8.2",
+	version="4.8.3",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
@@ -38,6 +38,8 @@ There is a mailing list available at https://groups.google.com/forum/?fromgroups
 
 
 *Latest Changes*:
+
+Version 4.8.3 updates the documentation.
 
 Version 4.8.2 makes the standard grid widget easier to customize by adding
 the custom_print_cell method.
